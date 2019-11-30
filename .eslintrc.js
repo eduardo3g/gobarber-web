8cc558f0c4744e5e8c590a2f77bea3901675b7f8
= <=>
2019-11-30 16:28:41 -0300

    chore: Private routes configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,7 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'no-unused-vars': ["error", { argsIgnorePattern: "^_" }],
     'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-props-no-spreading': 'off',
     'jsx-a11y/control-has-associated-label': 'off',
     'react-native/no-raw-text': 'off',
     'global-require': 'off',
---
+++ src/routes/Route.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate = false,
+  ...rest
+}) {
+  const signed = false;
+
+  // If user is not logged in and route is private, redirect to login page
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  // If user is logged and route is not private, redirect to dashboard
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return <Route {...rest} component={Component} />;
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
