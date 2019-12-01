29b4e62f1fd87078fdbb9975c88868e282214619
= <=>
2019-11-30 22:59:47 -0300

    fix: Infinite loading when user who's not a provider tries to login.

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -17,7 +18,9 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('Usuário não é prestador.');
+      // console.tron.error('Usuário não é prestador.');
+      toast.error('Usuário não é prestador de serviços');
+      yield put(signFailure());
       return;
     }
 
@@ -25,6 +28,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
