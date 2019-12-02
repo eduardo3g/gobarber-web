fc90940a67008d8746dd2b7c776b6d89333d653e
= <=>
2019-12-02 09:30:10 -0300

    chore: Profile picture.

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -7,12 +7,12 @@ import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, ...rest } = payload.data;
+    const { name, email, avatar_id, ...rest } = payload.data;
 
     // Object.assign = unir objetos
     // eslint-disable-next-line prefer-object-spread
     const profile = Object.assign(
-      { name, email },
+      { name, email, avatar_id },
       rest.oldPassword ? rest : {}
     );
 
